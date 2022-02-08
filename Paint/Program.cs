415de7d76a9806d9657dec5451e8ae58d7e49725
JJbrookes <jamesbrookes8@gmail.comgit config --global user.email>
2022-02-08 15:41:41 +0000

    Added Wall object

--- Paint/Program.cs
+++ Paint/Program.cs
@@ -11,46 +11,68 @@ namespace PaintCostCalculator
             do
             {
                 List<string> questions = new List<string>();
-                questions.Add("How many tins of paint did you use: ");
-                questions.Add("How much did each tin cost: £");
+                questions.Add("How many walls do you have: "); //0
+                questions.Add("What is the height of wall "); 
+                questions.Add("What is the width of wall ");
                 questions.Add("How long did it take to complete the job (Hours): ");
                 questions.Add("How Much do you want to charge per hour: £");
                 questions.Add("How much tax do you charge: ");
                 questions.Add("What type of paint are you using? (Enter 1 for Matte, 2 for Satin, 3 for Semi-gloss and 4 for High-gloss)");
 
-                float TotalTinsUsed = UserInputAndVaildFloat(questions[0]);
-                float CostPerTin = UserInputAndVaildInt(questions[5]);
-                switch (CostPerTin)
+                int AmountofWalls = UserInputAndVaildInt(questions[0]);
+                List<wall> walls = new List<wall>(AmountofWalls);
+                for (int i = 0; i < AmountofWalls; i++)
+                {
+                    walls.Add(new wall());
+                }
+
+                int WallNumber = 1;
+                float TotalArea = 0;
+                foreach (wall w in walls)
+                {
+
+                    w.width = UserInputAndVaildFloat(questions[1]+ WallNumber+ " (In Meters): ");
+                    w.height  = UserInputAndVaildFloat(questions[2] + WallNumber + " (In Meters): ");
+                    TotalArea += w.area;
+                    WallNumber++;
+
+                }
+                int TotalLiters = (int)Math.Ceiling(TotalArea) / 10; //This is based on a coverage of 10m² per litre of paint. 
+
+               // float TotalTinsUsed = UserInputAndVaildFloat(questions[0]);
+                float CostPerlitres = UserInputAndVaildInt(questions[6]);
+                switch (CostPerlitres)
                 {
                     case 1:
-                        CostPerTin = 2.99f;
+                        CostPerlitres = 2.99f;
                         break;
                     case 2:
-                        CostPerTin = 3.89f;
+                        CostPerlitres = 3.89f;
                         break;
                     case 3:
-                        CostPerTin = 4.99f;
+                        CostPerlitres = 4.99f;
                         break;
                     case 4:
-                        CostPerTin = 5.99f;
+                        CostPerlitres = 5.99f;
                         break;
                 }
-                float JobLengthHours = UserInputAndVaildFloat(questions[2]);
-                float HourlyRate = UserInputAndVaildFloat(questions[3]);
-                float TaxRate = UserInputAndVaildFloat(questions[4]);
+                float JobLengthHours = UserInputAndVaildFloat(questions[3]);
+                float HourlyRate = UserInputAndVaildFloat(questions[4]);
+                float TaxRate = UserInputAndVaildFloat(questions[5]);
 
-                double workingTotal = ((TotalTinsUsed * CostPerTin) + (JobLengthHours * HourlyRate));
+                double workingTotal = ((TotalLiters * CostPerlitres) + (JobLengthHours * HourlyRate));
                 if (workingTotal >= 500)
                 {
                     Console.WriteLine("10% Discount Given");
                     double discount = workingTotal * 0.1;
                     workingTotal = workingTotal - discount;
                 }
+
                 double tax = (workingTotal) * (TaxRate / 100);
                 double ChargeCustomer = workingTotal + tax;
                 Console.WriteLine("Charge Customer: £" + ChargeCustomer.ToString("0.00"));
 
-                Console.Write("Exit(0) Or Clear(1) or Countinue(1): ");
+                Console.Write("Exit(0) Or Clear(1) or Countinue(Enter): ");
                 string checkDone = Console.ReadLine();
                 switch (checkDone) 
                 {
@@ -58,14 +80,14 @@ namespace PaintCostCalculator
                         Console.WriteLine("Exiting...");
                         isDone = true;
                         break;
-                    default:
+                    case "1":
                         Console.Clear();
                         break;
-                }
-                 
-
+                    default:
+             
+                        break;
+                }                 
             } while (!isDone);
-
         }
 
         private static float UserInputAndVaildFloat(string Message)
@@ -92,5 +114,27 @@ namespace PaintCostCalculator
 
         }
     }
+
+    class wall
+    {
+        float WidthMeters;
+        float HeightMeters;
+
+        public float width   // property
+        {
+            get { return WidthMeters; }   // get method
+            set { WidthMeters = value; }  // set method
+        }
+        public float height // property
+        {
+            get { return HeightMeters; }   // get method
+            set { HeightMeters = value; }  // set method
+        }
+        public float area
+        {
+            get { return WidthMeters * HeightMeters ; }
+        }
+    }
+
 }
 
